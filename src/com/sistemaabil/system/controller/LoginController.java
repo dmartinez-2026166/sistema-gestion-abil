52927810e7ea919e6577decfe4a011ad4a19d3fe
Diego Martinez <dmartinez-2026166@kinal.edu.gt>
2026-09-09 16:10:31 -0600

    refactor: actualización del cambio de escena entre vista Login y vista Register

--- src/com/sistemaabil/system/controller/LoginController.java
+++ src/com/sistemaabil/system/controller/LoginController.java
@@ -1,11 +1,13 @@
 package com.sistemaabil.system.controller;
 
+import com.sistemaabil.system.utils.ViewFactory;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class LoginController {
     @FXML
@@ -25,4 +27,10 @@ public class LoginController {
 
     @FXML
     private TextField txtCorreo;
+    
+    @FXML
+    public void onRegister(MouseEvent event) {
+        ViewFactory viewFacto = new ViewFactory();
+        viewFacto.viewRegister();
+    }
 }
