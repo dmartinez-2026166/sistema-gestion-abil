abca607434423c816a921aedaac0f27ed9d4217a
Jefferson Arriola <>
2026-09-16 15:48:30 -0600

    feat: se agrego el panel de control

--- src/com/sistemaabil/system/controller/LoginController.java
+++ src/com/sistemaabil/system/controller/LoginController.java
@@ -4,10 +4,27 @@
  */
 package com.sistemaabil.system.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import com.sistemaabil.system.utils.ViewFactory;
+
 /**
  *
  * @author diego
  */
-public class LoginController {
+public class LoginController implements Initializable {
     
+         @Override
+         public void initialize(URL url, ResourceBundle rb) {
+        
+         }
+         
+        @FXML
+        public void onRegister(MouseEvent event){
+        ViewFactory viewFacto = new ViewFactory();
+        viewFacto.viewRegister();
+    }
 }
