87bc76fec1d79030e5f80a256a89d3d7a67a535a
Diego Martínez <dmartinez-2026166@kinal.edu.gt>
2026-09-20 15:34:46 -0600

    refactor: se actualizaron los archivos base de la rama

---
+++ src/com/sistemaabil/system/config/Enviroment.java
@@ -0,0 +1,8 @@
+package com.sistemaabil.system.config;
+
+public class Enviroment {
+    protected final static String USER = "IN4AV";
+    protected static final String PASSWORD = "dmartinez2026166";
+    protected static final String DATA_BASE = "sistema_gestion_abil_in4av";
+    protected static final String LOCATION_SERVICE = "localhost: 3306";
+}
